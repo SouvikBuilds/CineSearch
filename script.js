7f935afa27386608e4a97ad3687d5f8962364326
Souvik Chatterjee <bravoth0729v@gmail.com>
2025-07-15 10:26:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,16 +14,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const response = await fetch(apiUrl)
         const data = await response.json()
+        console.log(data);
 
         if (data.Response === "True") {
             document.getElementById("movie-name").innerText = data.Title
-            document.getElementById("movie-poster").src = data.Poster
+            if (data.Poster !== "N/A") {
+                document.getElementById("movie-poster").src = data.Poster;
+            } else {
+                document.getElementById("movie-poster").src = "https://via.placeholder.com/300x450?text=No+Image";
+            }
+
             document.getElementById("movie-released").innerText = "Released: " + data.Released
             document.getElementById("movie-genre").innerText = "Genre: " + data.Genre
             document.getElementById("imdb-movie-rating").innerText = "IMDB: " + data.imdbRating
             document.getElementById("metacritic-movie-rating").innerText = "Metacritic: " + data.Metascore + "/100"
             document.getElementById("movie-plot").innerText = "plot: " + data.Plot
             document.getElementById("box-office-collection").innerText = "Box Office Collection: " + data.BoxOffice
+
+
 
             document.querySelector(".movie-rating").style.display = "flex"
         } else {
