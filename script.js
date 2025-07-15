d5c9984400ccaf343442d3f00ede0c2c776d7d04
Souvik Chatterjee <bravoth0729v@gmail.com>
2025-07-15 10:17:29 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const showBtn = document.getElementById("show-movie")
+
+    async function showMovieDetails() {
+        const movieName = document.getElementById("search-movie").value.trim()
+
+        if (movieName === '') {
+            alert("Please Enter A Movie Name")
+            return
+        }
+
+        const apikey = "41986ec8"
+        const apiUrl = `https://www.omdbapi.com/?t=${movieName}&apikey=${apikey}`
+
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+
+        if (data.Response === "True") {
+            document.getElementById("movie-name").innerText = data.Title
+            document.getElementById("movie-poster").src = data.Poster
+            document.getElementById("movie-released").innerText = "Released: " + data.Released
+            document.getElementById("movie-genre").innerText = "Genre: " + data.Genre
+            document.getElementById("imdb-movie-rating").innerText = "IMDB: " + data.imdbRating
+            document.getElementById("metacritic-movie-rating").innerText = "Metacritic: " + data.Metascore + "/100"
+            document.getElementById("movie-plot").innerText = "plot: " + data.Plot
+            document.getElementById("box-office-collection").innerText = "Box Office Collection: " + data.BoxOffice
+
+            document.querySelector(".movie-rating").style.display = "flex"
+        } else {
+            alert("Movie Not Found")
+        }
+    }
+    showBtn.addEventListener("click", showMovieDetails)
+})
